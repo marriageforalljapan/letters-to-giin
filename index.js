628450ea8bf643435f73137a75e5a917120c99db
marriageforalljapan <137747486+marriageforalljapan@users.noreply.github.com>
2023-07-10 21:12:04 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,268 @@
+function fetchJSON(url, callback) {
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then(data => callback(data))
+    .catch(error => console.error(error));
+}
+
+function createCards(data, startIndex, endIndex) {
+  var cardsContainer = document.getElementById("cardsContainer");
+  var cardTemplate = document.getElementById("cardTemplate").content;
+
+  for (var i = startIndex; i < endIndex; i++) {
+    var item = data[i];
+    var cardElement = cardTemplate.cloneNode(true).querySelector(".card");
+    cardElement.querySelector("[data-name]").textContent = item["氏名"];
+    cardElement.querySelector("[data-kana]").textContent = item["かな"];
+    cardElement.querySelector("[data-house]").textContent = item["衆参"];
+    cardElement.querySelector("[data-senkyoku]").textContent = item["選挙区"];
+    cardElement.querySelector("[data-party]").textContent = item["party"];
+    cardElement.querySelector("[data-sanpi]").textContent = item["賛否"];
+    cardElement.querySelector("[data-zipcode]").textContent = item["国会_郵便番号"];
+    cardElement.querySelector("[data-address]").textContent = item["国会_住所"];
+    cardElement.querySelector("[data-prefecture]").textContent = item["都道府県"];
+    cardElement.querySelector("[data-area]").textContent = item["区"];
+    cardElement.querySelector("[data-page]").setAttribute("href", item["個別ページ"]);
+    cardElement.querySelector("[data-photo]").setAttribute("src", item["img"]);
+
+    var sanpiElement = cardElement.querySelector("[data-sanpi]");
+    var sanpiValue = item["賛否"];
+    sanpiElement.textContent = sanpiValue.replace(/^\d+\./, ''); // 数字部分を削除して表示
+
+    // 賛否に応じてテキスト色を設定
+    if (sanpiValue === "1.賛成") {
+      sanpiElement.style.color = "red";
+    } else if (sanpiValue === "2.どちらかと言えば賛成") {
+      sanpiElement.style.color = "lightcoral";
+    } else if (sanpiValue === "3.どちらとも言えない") {
+      sanpiElement.style.color = "dimgray";
+    } else if (sanpiValue === "4.どちらかと言えば反対") {
+      sanpiElement.style.color = "royalblue";
+    } else if (sanpiValue === "5.反対") {
+      sanpiElement.style.color = "mediumblue";
+    } else if (sanpiValue === "6.無回答") {
+      sanpiElement.style.color = "grey";
+    }
+
+    cardsContainer.appendChild(cardElement);
+  }
+}
+
+function clearCards() {
+  var cardsContainer = document.getElementById("cardsContainer");
+  while (cardsContainer.firstChild) {
+    cardsContainer.removeChild(cardsContainer.firstChild);
+  }
+}
+
+var jsonData; // JSONデータを保持する変数
+var maxItemsToShow = 20; // 一度に表示する最大項目数
+var currentIndex = 0; // 現在のインデックス
+var currentFilters = {
+  name: "",
+  house: "",
+  prefecture: "",
+  party: "",
+  sanpi: ""
+};
+
+function fetchFilteredData() {
+  var url = "https://raw.githubusercontent.com/marriageforalljapan/letters-to-giin/main/giin.json";
+  var filteredUrl = url + "?" + Date.now(); // リクエストをキャッシュしないためにクエリパラメータを追加
+
+  fetchJSON(filteredUrl, function(data) {
+    jsonData = data; // データをjsonData変数に代入
+    applyFilters();
+  });
+}
+
+function showMoreItems() {
+  var startIndex = currentIndex; // 現在のインデックスを保持
+  var endIndex = currentIndex + maxItemsToShow; // endIndex を計算
+
+  if (endIndex > jsonDataFiltered.length) {
+    endIndex = jsonDataFiltered.length; // endIndex を調整
+  }
+
+  createCards(jsonDataFiltered, startIndex, endIndex);
+
+  currentIndex = endIndex; // インデックスを更新
+
+  var moreButtonContainer = document.getElementById("moreButtonContainer");
+  if (currentIndex < jsonDataFiltered.length) {
+    moreButtonContainer.style.display = "block"; // 「もっと見る」ボタンを表示
+  } else {
+    moreButtonContainer.style.display = "none"; // 「もっと見る」ボタンを非表示
+  }
+}
+
+
+function applyFilters() {
+  currentIndex = maxItemsToShow;
+  clearCards();
+
+  var filteredData = jsonData.filter(function(item) {
+    var nameMatch = currentFilters.name === "" || item["氏名"].toLowerCase().includes(currentFilters.name);
+    var houseMatch = currentFilters.house === "" || item["衆参"].includes(currentFilters.house);
+    var prefectureMatch = currentFilters.prefecture === "" || item["都道府県"] === currentFilters.prefecture;
+    var partyMatch = currentFilters.party === "" || item["party"].includes(currentFilters.party);
+    var sanpiMatch = currentFilters.sanpi === "" || item["賛否"].includes(currentFilters.sanpi);
+    return nameMatch && houseMatch && prefectureMatch && sanpiMatch && partyMatch;
+  });
+
+  jsonDataFiltered = filteredData; // フィルター後のデータを保持
+
+  createCards(filteredData, 0, Math.min(maxItemsToShow, filteredData.length));
+
+  var moreButtonContainer = document.getElementById("moreButtonContainer");
+  if (currentIndex < filteredData.length) {
+    moreButtonContainer.style.display = "block"; // 「もっと見る」ボタンを表示
+  } else {
+    moreButtonContainer.style.display = "none"; // 「もっと見る」ボタンを非表示
+  }
+}
+
+
+// ボタンのクリック時にスタイルを設定する関数
+function setButtonStyle(button) {
+  var activeButtons = document.getElementsByClassName("active");
+  for (var i = 0; i < activeButtons.length; i++) {
+    activeButtons[i].classList.remove("active");
+  }
+  button.classList.add("active");
+}
+
+function searchByName() {
+  var searchInput = document.getElementById("searchInput");
+  currentFilters.name = searchInput.value.trim().toLowerCase();
+  applyFilters();
+}
+
+
+function filterByPrefecture() {
+  var filterPrefecture = document.getElementById("filterPrefecture").value;
+  currentFilters.prefecture = filterPrefecture; // 都道府県のフィルターを選択
+  fetchFilteredData();
+}
+
+function filterByHouse(house) {
+  clearCards(); // 表示中のカードをクリア
+  if (currentFilters.house === house) {
+    currentFilters.house = ""; // 同じボタンがクリックされた場合はフィルターを解除
+  } else {
+    currentFilters.house = house; // フィルターを選択
+  };
+  fetchFilteredData();
+
+  var houseButtons = document.querySelectorAll("#filterbyhouse button");
+  houseButtons.forEach(function (button) {
+    if (button.textContent === house) {
+      button.classList.toggle("active-house"); // 選択中のボタンにactive-houseクラスを追加・削除
+    } else {
+      button.classList.remove("active-house"); // 選択されていないボタンからactive-houseクラスを削除
+    }
+  });
+}
+
+function filterByParty(party) {
+  clearCards(); // 表示中のカードをクリア
+  if (currentFilters.party === party) {
+    currentFilters.party = ""; // 同じボタンがクリックされた場合はフィルターを解除
+  } else {
+    currentFilters.party = party; // フィルターを選択
+  };
+  fetchFilteredData();
+  
+  var partyButtons = document.querySelectorAll("#filterbyparty button");
+  partyButtons.forEach(function (button) {
+    if (button.textContent === party) {
+      button.classList.toggle("active-party"); // 選択中のボタンにactive-partyクラスを追加・削除
+    } else {
+      button.classList.remove("active-party"); // 選択されていないボタンからactive-partyクラスを削除
+    }
+  });
+}
+
+function filterBySanpi(sanpi) {
+  clearCards(); // 表示中のカードをクリア
+  if (currentFilters.sanpi === sanpi) {
+    currentFilters.sanpi = ""; // 同じボタンがクリックされた場合はフィルターを解除
+  } else {
+    currentFilters.sanpi = sanpi; // フィルターを選択
+  };
+  fetchFilteredData();
+  
+  var sanpiButtons = document.querySelectorAll("#filterbysanpi button");
+  sanpiButtons.forEach(function (button) {
+    if (button.textContent === sanpi) {
+      button.classList.toggle("active-sanpi"); // 選択中のボタンにactive-sanpiクラスを追加・削除
+    } else {
+      button.classList.remove("active-sanpi"); // 選択されていないボタンからactive-sanpiクラスを削除
+    }
+  });
+}
+
+function resetSearchAndFilter() {
+  var searchInput = document.getElementById("searchInput");
+  searchInput.value = ""; // 検索入力をクリア
+
+  var filterPrefecture = document.getElementById("filterPrefecture");
+  filterPrefecture.value = ""; // 都道府県の選択をリセット
+
+  var houseButtons = document.querySelectorAll("#filterbyhouse button");
+  houseButtons.forEach(function (button) {
+      button.classList.remove("active-house"); // active-houseクラスを削除
+  });
+
+  var partyButtons = document.querySelectorAll("#filterbyparty button");
+  partyButtons.forEach(function (button) {
+      button.classList.remove("active-party"); // active-partyクラスを削除
+  });
+
+  var sanpiButtons = document.querySelectorAll("#filterbysanpi button");
+  sanpiButtons.forEach(function (button) {
+      button.classList.remove("active-sanpi"); // active-sanpiクラスを削除
+  });
+
+  currentIndex = maxItemsToShow;
+  currentFilters.name = "";
+  currentFilters.house = "";
+  currentFilters.prefecture = "";
+  currentFilters.party = "";
+  currentFilters.sanpi = ""; // 賛否フィルターをリセット
+
+  clearCards();
+  jsonDataFiltered = jsonData; // データをリセット
+  createCards(jsonDataFiltered, 0, maxItemsToShow);
+
+  var moreButtonContainer = document.getElementById("moreButtonContainer");
+  if (currentIndex < jsonDataFiltered.length) {
+    moreButtonContainer.style.display = "block"; // 「もっと見る」ボタンを表示
+  } else {
+    moreButtonContainer.style.display = "none"; // 「もっと見る」ボタンを非表示
+  }
+}
+
+fetchFilteredData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function initAutocomplete() {
     //マップの初期設定
     const map = new google.maps.Map(document.getElementById("map"), {
